2074311c1cac81fdb5a1f60737d8ffc25a227b4b
Mohamed-Abdalla1 <mohamedfathalla2772@gmail.com>
2024-09-06 15:17:39 +0300

    success view done

--- lib/features/Authentication/screens/signUp/succcess_view.dart
+++ lib/features/Authentication/screens/signUp/succcess_view.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_core/get_core.dart';
-import 'package:get/get_navigation/get_navigation.dart';
+import 'package:get/get.dart';
+import 'package:shoppy/core/common/styles/Tspacing_style.dart';
 import 'package:shoppy/core/utils/constants/image_strings.dart';
 import 'package:shoppy/core/utils/constants/sizes.dart';
 import 'package:shoppy/core/utils/constants/text_strings.dart';
 import 'package:shoppy/core/utils/device/device_utility.dart';
 import 'package:shoppy/features/Authentication/screens/login/login_view.dart';
 
-class VerifyEmailView extends StatelessWidget {
-  const VerifyEmailView({super.key});
+class SucccessView extends StatelessWidget {
+  const SucccessView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return  Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         actions: [
@@ -23,12 +23,12 @@ class VerifyEmailView extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          padding:  TSpacingStyle.paddingWithAppBarHeight * 2,
           child: Column(
             children: [
               //image
               Image(
-                image: const AssetImage(TImages.deliveredEmailIllustration),
+                image: const AssetImage(TImages.staticSuccessIllustration),
                 width: TDeviceUtils.getScreenWidth(context) * 0.6,
               ),
               const SizedBox(
@@ -36,15 +36,11 @@ class VerifyEmailView extends StatelessWidget {
               ),
               //title & subTitle
               Text(
-                TTexts.confirmEmail,
+                TTexts.yourAccountCreatedTitle,
                 style: Theme.of(context).textTheme.labelLarge,
               ),
               Text(
-                'Mopoooo@gmail.com',
-                style: Theme.of(context).textTheme.labelMedium,
-              ),
-              Text(
-                TTexts.confirmEmailSubTitle,
+                TTexts.yourAccountCreatedSubTitle,
                 style: Theme.of(context).textTheme.labelLarge,
               ),
               //Buttons
@@ -52,14 +48,6 @@ class VerifyEmailView extends StatelessWidget {
                 width: double.infinity,
                 child: ElevatedButton(
                     onPressed: () {}, child: const Text(TTexts.tContinue)),
-              ),
-              const SizedBox(
-                height: TSizes.spaceBtwInputFields,
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: TextButton(
-                    onPressed: () {}, child: const Text(TTexts.resendEmail)),
               ),
             ],
           ),
