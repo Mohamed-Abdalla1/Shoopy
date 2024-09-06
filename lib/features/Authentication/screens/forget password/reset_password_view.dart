8850d26f2cd599eade19a609462baa73130830bb
Mohamed-Abdalla1 <mohamedfathalla2772@gmail.com>
2024-09-06 15:42:54 +0300

    adding image to rest password view

--- lib/features/Authentication/screens/forget password/reset_password_view.dart
+++ lib/features/Authentication/screens/forget password/reset_password_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:shoppy/core/utils/constants/image_strings.dart';
+import 'package:shoppy/core/utils/constants/sizes.dart';
+import 'package:shoppy/core/utils/device/device_utility.dart';
 
 class ResetPasswordView extends StatelessWidget {
   const ResetPasswordView({super.key});
@@ -13,6 +16,21 @@ class ResetPasswordView extends StatelessWidget {
         actions: [
           IconButton(onPressed: () => Get.back(), icon:const  Icon(Iconsax.backward))
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              //image
+              Image(image: AssetImage(TImages.deliveredInPlaneIllustration),width: TDeviceUtils.getScreenWidth(context) *0.6,),
+             const SizedBox(height: TSizes.spaceBtwSections,)
+              //title &subTitle
+              //btn
+            ],
+          ),
+        ),
+        
       ),
     );
   }
