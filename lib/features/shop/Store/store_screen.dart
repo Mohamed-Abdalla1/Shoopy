5aec6cba15ab1e179f55fba61f4182132c96a69f
Mohamed-Abdalla1 <mohamedfathalla2772@gmail.com>
2025-01-09 06:10:06 +0200

    at the bottom we adding 5 tabs

--- lib/features/shop/Store/store_screen.dart
+++ lib/features/shop/Store/store_screen.dart
@@ -17,106 +17,127 @@ class StoreScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(
-        showBackArrow: false,
-        title: Text(
-          'Store',
-          style: Theme.of(context).textTheme.headlineMedium,
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: CustomAppBar(
+          showBackArrow: false,
+          title: Text(
+            'Store',
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          actions: [
+            CartCounterIcon(onPressed: () {}, color: TColors.primary),
+          ],
         ),
-        actions: [
-          CartCounterIcon(onPressed: () {}, color: TColors.primary),
-        ],
-      ),
-      body: NestedScrollView(
-          headerSliverBuilder: (context, innerBoxIsScrolled) {
-            return [
-              SliverAppBar(
-                pinned: true,
-                floating: true,
-                backgroundColor: THelperFunctions.isDarkMode(context)
-                    ? TColors.black
-                    : TColors.white,
-                expandedHeight: 440,
-                automaticallyImplyLeading: false,
-                flexibleSpace: Padding(
-                  padding: const EdgeInsets.all(TSizes.defaultSpace),
-                  child: ListView(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    children: [
-                      const SizedBox(
-                        height: TSizes.spaceBtwItems / 2,
-                      ),
-                      //Search container
-                      const SearchContainer(
-                        padding: 0,
-                      ),
-                      const SizedBox(
-                        height: TSizes.spaceBtwItems,
-                      ),
-                      //featured brands
-                      HeadingSection(
-                        title: 'Featured Brands',
-                        textColor: THelperFunctions.isDarkMode(context)
-                            ? TColors.white
-                            : TColors.black,
-                        onPressed: () {},
-                        showActionButton: true,
-                      ),
-                      const SizedBox(
-                        height: TSizes.spaceBtwItems,
-                      ),
-                      TGridViewLayout(
-                        mainAxisExtent: 80,
-                        itemCount: 4,
-                        itemBuilder: (context, index) {
-                          return TroundedContainer(
-                            padding: const EdgeInsets.all(TSizes.sm),
-                            showBorder: true,
-                            backGroundColor: Colors.transparent,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Flexible(
-                                  child: TCircularImage(
-                                    image: TImages.clothIcon,
-                                    overlayColor:
-                                        THelperFunctions.isDarkMode(context)
-                                            ? TColors.white
-                                            : TColors.black,
+        body: NestedScrollView(
+            headerSliverBuilder: (context, innerBoxIsScrolled) {
+              return [
+                SliverAppBar(
+                  pinned: true,
+                  floating: true,
+                  backgroundColor: THelperFunctions.isDarkMode(context)
+                      ? TColors.black
+                      : TColors.white,
+                  expandedHeight: 440,
+                  automaticallyImplyLeading: false,
+                  flexibleSpace: Padding(
+                    padding: const EdgeInsets.all(TSizes.defaultSpace),
+                    child: ListView(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      children: [
+                        const SizedBox(
+                          height: TSizes.spaceBtwItems / 2,
+                        ),
+                        //Search container
+                        const SearchContainer(
+                          padding: 0,
+                        ),
+                        const SizedBox(
+                          height: TSizes.spaceBtwItems,
+                        ),
+                        //featured brands
+                        HeadingSection(
+                          title: 'Featured Brands',
+                          textColor: THelperFunctions.isDarkMode(context)
+                              ? TColors.white
+                              : TColors.black,
+                          onPressed: () {},
+                          showActionButton: true,
+                        ),
+                        const SizedBox(
+                          height: TSizes.spaceBtwItems,
+                        ),
+                        TGridViewLayout(
+                          mainAxisExtent: 80,
+                          itemCount: 4,
+                          itemBuilder: (context, index) {
+                            return TroundedContainer(
+                              padding: const EdgeInsets.all(TSizes.sm),
+                              showBorder: true,
+                              backGroundColor: Colors.transparent,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Flexible(
+                                    child: TCircularImage(
+                                      image: TImages.clothIcon,
+                                      overlayColor:
+                                          THelperFunctions.isDarkMode(context)
+                                              ? TColors.white
+                                              : TColors.black,
+                                    ),
                                   ),
-                                ),
-                                Expanded(
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      const TBrandTitleWithVerifiedIcon(
-                                          title: 'Nike'),
-                                      Text(
-                                        '265 Products',
-                                        overflow: TextOverflow.ellipsis,
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .labelMedium,
-                                      )
-                                    ],
-                                  ),
-                                )
-                              ],
-                            ),
-                          );
-                        },
-                      )
+                                  Expanded(
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        const TBrandTitleWithVerifiedIcon(
+                                            title: 'Nike'),
+                                        Text(
+                                          '265 Products',
+                                          overflow: TextOverflow.ellipsis,
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .labelMedium,
+                                        )
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            );
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                  bottom: TabBar(
+                    tabAlignment: TabAlignment.start,
+                    isScrollable: true,
+                    indicatorColor: TColors.primary,
+                    labelColor: THelperFunctions.isDarkMode(context)
+                        ? TColors.white
+                        : TColors.primary,
+                    unselectedLabelColor: TColors.darkGrey,
+                    tabs: [
+                      Tab(
+                        child: Text('sport'),
+                      ),
+                      Tab(child: Text('cloths')),
+                      Tab(child: Text('cosmetics')),
+                      Tab(child: Text('furniture')),
+                      Tab(child: Text('electoincs')),
                     ],
                   ),
-                ),
-              )
-            ];
-          },
-          body: Column()),
+                )
+              ];
+            },
+            body: Column()),
+      ),
     );
   }
 }
